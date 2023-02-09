b0e9ee260632728f7bd34b5e02a3686ace4ccfd4
luka <l.lortkifanidze@noxtton.com>
2023-02-09 18:38:30 +0400

    synonyms and antonyms

--- sentiments.py
+++ sentiments.py
@@ -1,3 +1,7 @@
+import re
+import nltk
+from nltk.corpus import wordnet
+
 def dummy_sentiment(text,pos,neg):
     punc_free = re.sub(r'[^\w\s]', ' ', text.lower())
     pos_low = [w.lower() for w in pos]
@@ -11,3 +15,19 @@ def dummy_sentiment(text,pos,neg):
         elif word in neg_low:
             negatives.append(word)
     return { 'positives' : positives,'pos_count' : len(positives), 'negatives' : negatives, 'neg_count' : len(negatives) }
+
+
+def get_antonyms(word):
+    antonyms = []
+    for syn in wordnet.synsets("good"):
+        for l in syn.lemmas():
+            if l.antonyms():
+                antonyms.append(l.antonyms()[0].name())
+    return antonyms
+
+def get_synonyms(word):
+    synonyms = []
+    for syn in wordnet.synsets("good"):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+    return synonyms
