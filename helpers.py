582f58b3c3ddad43bc121e1c6da69fb50d69bf5f
luka <l.lortkifanidze@noxtton.com>
2023-02-09 18:56:42 +0400

    helper fucntions

---
+++ helpers.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.corpus import wordnet
+from words_forms import words
+
+def get_antonyms(word):
+    antonyms = []
+    for syn in wordnet.synsets("good"):
+        for l in syn.lemmas():
+            if l.antonyms():
+                antonyms.append(l.antonyms()[0].name())
+    return antonyms
+
+def get_synonyms(word):
+    synonyms = []
+    for syn in wordnet.synsets("good"):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+    return synonyms
+
+
+def get_word_forms(word):
+    try:
+        return words[word]
+    except KeyError:
+        return []
